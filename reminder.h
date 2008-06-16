2ed3800432f46d8015fe13fb0981169f620c83d0
Riccardo Iaconelli <riccardo@kde.org>
2008-06-16 15:43:21 +0200

    Skeleton class to show reminders: in!
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

---
+++ reminder.h
@@ -0,0 +1,24 @@
+/*
+  Copyright (c) 2008 Riccardo Iaconelli <riccardo@kde.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+*/
+
+
+#ifdef REMINDER_H
+#define REMINDER_H
+
+#include <QGraphicsWidget>
+
+class Reminder : public QGraphicsWidget
+{
+
+
+};
+
+
+#endif
+
